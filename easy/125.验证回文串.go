416ac21270ba277f2f027fa5ded88c978704f27c
zhaoxfan98 <zhaoxfan98@gmail.com>
2021-08-19 11:29:36 +0800

    8/19

---
+++ easy/125.验证回文串.go
@@ -0,0 +1,25 @@
+package easy
+
+import "strings"
+
+func isPalindrome(s string) bool {
+	//筛选+判断
+	var str string
+	for i := 0; i < len(s); i++ {
+		if isalnum(s[i]) {
+			str += string(s[i])
+		}
+	}
+	n := len(str)
+	str = strings.ToUpper(str)
+	for i := 0; i < n/2; i++ {
+		if str[i] != str[n-i-1] {
+			return false
+		}
+	}
+	return true
+}
+
+func isalnum(ch byte) bool {
+	return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')
+}
