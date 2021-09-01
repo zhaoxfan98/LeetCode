89dab375a971017e789cce65cf6f505fd7aadf72
yfzhao <m18810617098@163.com>
2021-09-01 18:59:50 +0800

    9/1

---
+++ medium/165.比较版本号.go
@@ -0,0 +1,54 @@
+package medium
+
+import (
+	"strconv"
+	"strings"
+)
+
+//字符串分割
+func compareVersion1(version1 string, version2 string) int {
+	v1 := strings.Split(version1, ".")
+	v2 := strings.Split(version2, ".")
+	for i := 0; i < len(v1) || i < len(v2); i++ {
+		x, y := 0, 0
+		if i < len(v1) {
+			x, _ = strconv.Atoi(v1[i])
+		}
+		if i < len(v2) {
+			y, _ = strconv.Atoi(v2[i])
+		}
+		if x < y {
+			return -1
+		}
+		if x > y {
+			return 1
+		}
+	}
+	return 0
+}
+
+//双指针  优化空间复杂度 可以在分割版本号的同时解析出修订号进行比较
+
+func compareVersion2(version1 string, version2 string) int {
+	n, m := len(version1), len(version2)
+	i, j := 0, 0
+	for i < n || j < m {
+		x := 0
+		for ; i < n && version1[i] != '.'; i++ {
+			x = x*10 + int(version1[i]-'0')
+		}
+		i++ //跳过点号
+		y := 0
+		for ; j < m && version2[j] != '.'; j++ {
+			y = y*10 + int(version2[j]-'0')
+		}
+		j++
+		if x > y {
+			return 1
+		}
+		if x < y {
+			return -1
+		}
+	}
+	return 0
+}
