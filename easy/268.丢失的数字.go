662bc4e140516b6014f2a3b1d6f48130533e3d1d
yfzhao <m18810617098@163.com>
2021-09-03 00:15:49 +0800

    9/3

---
+++ easy/268.丢失的数字.go
@@ -0,0 +1,13 @@
+package easy
+
+import "sort"
+
+func missingNumber(nums []int) int {
+	sort.Ints(nums)
+	for i := 0; i < len(nums); i++ {
+		if i != nums[i] {
+			return i
+		}
+	}
+	return len(nums)
+}
